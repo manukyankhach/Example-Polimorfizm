314b51be2929e30f9a61642482d9eb1cdf93f3ed
manukyankhach <83696364+manukyankhach@users.noreply.github.com>
2022-05-06 11:04:13 +0400

    Add project files.

---
+++ Polimorfizm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticPolimorfizm polimorfizm = new StaticPolimorfizm();
+            polimorfizm.Calculate(100, 300);
+            polimorfizm.Calculate(100, 300, true);
+            Console.ReadLine();
+        }
+    }
+
+    public class StaticPolimorfizm
+    {
+        public void Calculate(double firstParameter, double secondParameter)
+        {
+            double result = firstParameter + secondParameter;
+            Console.WriteLine($"Result of Calculate method without Bool Value - {result}");
+        }
+        public void Calculate(double firstParameter, double secondParameter, bool value)
+        {
+            double result = firstParameter + secondParameter + 1000;
+            Console.WriteLine($"Result of Calculate method from Bool Value - {result}");
+        }
+    }
+}
